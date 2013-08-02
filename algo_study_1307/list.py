1d4c27fb35889e0c35bcb70dace338e9ebfee2b2
Devgrapher <devgrapher@gmail.com>
2013-08-02 18:48:20 +0900

    insert Ã߰¡

--- algo_study_1307/list.py
+++ algo_study_1307/list.py
@@ -1,18 +1,35 @@
 class List():
-	def __init__(self, data):
-		self.data_ = data
-		self.next_ = None
-	def __init__(self, data, next):
+	def __init__(self, data = None, next = None):
 		self.data_ = data
 		self.next_ = next
+
 	def __iter__(self):
 		return ListIter(self)
+
 	def next(self):
 		return self.next_
+
 	def data(self):
 		return self.data_
-	def append(self, next):
-		self.next_ = next
+
+	def insert(self, index, data):
+		# TODO: check length
+		if self.data_ is None:
+			self.data_ = data
+			return
+		elif index is 0:
+			next = List(self.data_, self.next_)
+			self.__init__(data, next)
+			return 
+
+		begin = List(None, self)
+		it = begin
+		for i in range(index):
+			it = it.next()
+
+		it.next_ = List(data, it.next_)
+		return
+
 	def find(self, data):
 		t = self
 		while t is not None:
@@ -31,17 +48,19 @@ class ListIter():
 			data = self.target.data()
 			self.target = self.target.next()
 			return data
+	def next_iter(self):
+		return self.target.next_.__iter__()
 
 
-begin = None
+begin = List()
 for i in range(10, 0, -1):
-	begin = List(i, begin)
+	begin.insert(0, i)
 
 t = begin
 while t is not None:
 	print(t.data())
 	t = t.next()
-
+begin.insert(3,111)
 t = begin
 for i in t:
 	print(i)
